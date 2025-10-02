bde5375c0024713be8f9b793ff5e0c38bc6620d1
Gaston Fontagneu <gfontagn@f6r13s1.clusters.42paris.fr>
2025-10-02 20:08:50 +0200

    code decoupe en plusieurs fonctions. Il faut maintenant en faire des methodes

--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,8 @@
 #include "../webserv.hpp"
 
-int	global_function() {
-
-	// SERVER-SIDE
+int	create_server_socket(void) {
 	
-	// 1. Create a socket by specifying:
+	// Create a socket by specifying:
 	//	- domain / adress family (Ipv4, AppleTalk ...), 
 	//	- semantics / type / unit (Streams, datagrams ...) 
 	//	- protocol: dependent of domain + type
@@ -17,8 +15,13 @@ int	global_function() {
 	
     int opt = 1;
     setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	
+	return server_socket;
+}
 
-	// 2. Bind it to a local IP / port
+void	bind_server_socket(int server_socket) {
+	
+	// Bind the socket to a local IP / port
 	
 	struct addrinfo hints, *res;
 
@@ -26,7 +29,7 @@ int	global_function() {
 
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;	// + NULL node -> suitable for bind()
+	hints.ai_flags = AI_PASSIVE;
 
 	// allocate res with a linked list containing all interfaces (ip adresses):
 	//	- 127.0.0.1 / localhost
@@ -38,96 +41,120 @@ int	global_function() {
 	if (status)
 		throw std::runtime_error(gai_strerror(status));
 	
-	// 3. bind the socket with the first adress of the list and mark it as passive
-	
 	if (bind(server_socket, res->ai_addr, res->ai_addrlen) == -1)
 		throw std::runtime_error(strerror(errno));
 
-	freeaddrinfo(res);	// res is not freed if an exception is thrown
+	freeaddrinfo(res);	// /!\ res is not freed if an exception is thrown
+}
+
+void	create_connected_socket(int server_socket, struct pollfd poll_fd[MAX_CLIENTS], int *nfds) {
+
+	// if server socket receives data, create a new connected socket 
+
+	sockaddr_in	client;
+	memset(&client, 0, sizeof(client));
+	socklen_t	client_size = sizeof(client);
+	
+
+	int	connected_socket = accept(server_socket, (sockaddr *)&client, &client_size);
+	if (connected_socket == -1)
+		throw std::runtime_error(strerror(errno));
+
+	if (*nfds < MAX_CLIENTS) {
+		poll_fd[*nfds].fd = connected_socket;
+		poll_fd[*nfds].events = POLLIN;
+		(*nfds)++;
+		std::cout << "A new client is on the server !" << std::endl;
+	}
+	else {
+		std::cerr << "Too many clients, last one has been disconnected" << std::endl;
+		close(connected_socket);
+	}
+}
+
+// This function should be defined as the method of a class to have all the attributes accessible (like server socket) to make complex actions
+
+void	process_message(struct pollfd client_fd, char buff[BUFF_SIZE], int bytes_received) {
+
+	std::cout << "Client says: " << buff << std::endl;
+	send(client_fd.fd, buff, bytes_received + 1, 0); // (or write())
+}
+
+void	process_clients(struct pollfd poll_fd[MAX_CLIENTS], int *nfds) {
+	
+	// if a connected socket receives data, THEN we should do stuff with it
+
+	char			buff[BUFF_SIZE];	
+
+	for (int i = 1; i < *nfds; i++) {
+
+		if (poll_fd[i].revents & POLLIN) {
+
+			memset(buff, 0, sizeof(buff));
+
+			int	bytes_received = recv(poll_fd[i].fd, buff, sizeof(buff), 0); // (or read())
+
+			if (bytes_received < 0)
+				throw std::runtime_error(strerror(errno));
+
+			if (bytes_received == 0) {
+				std::cout << "client " << i << " disconnected" << std::endl;
+				close(poll_fd[i].fd);
+				poll_fd[i] = poll_fd[*nfds - 1];
+				(*nfds)--;
+				i--;
+			}
+			else {
+				process_message(poll_fd[i], buff, bytes_received);
+			}
+		}
+	}
+}
+
+void	run_server(int server_socket) {
+
+	int				nfds = 1;
+	struct pollfd	poll_fd[MAX_CLIENTS];
+
+	poll_fd[0].fd = server_socket;
+	poll_fd[0].events = POLLIN;
 
 	if (listen(server_socket, MAX_QUEUE))
 		throw std::runtime_error(strerror(errno));
 
-	// 4. watch over the socket
-	
-	struct pollfd	poll_fd[MAX_CLIENTS];
-	poll_fd[0].fd = server_socket;
-	poll_fd[0].events = POLLIN;
-
-	int		nfds = 1;
-	char	buff[BUFF_SIZE];	
-
-
 	while (true) {
 
-		// poll blocks execution until something happens, but it watches all the sockets at once 
-		// if poll returns, something happened on at least one socket, we must find which one
-		
 		if (poll(poll_fd, nfds, -1) == -1)
 			throw std::runtime_error(strerror(errno));
 
-		if (poll_fd[0].revents & POLLIN) {
-			// if server socket receives data, create a new connected socket 
+		if (poll_fd[0].revents & POLLIN)
+			create_connected_socket(server_socket, poll_fd, &nfds);
 
-			sockaddr_in	client;
-			memset(&client, 0, sizeof(client));
-			socklen_t	client_size = sizeof(client);
-			
-
-			int	connected_socket = accept(server_socket, (sockaddr *)&client, &client_size);
-			if (connected_socket == -1)
-				throw std::runtime_error(strerror(errno));
-
-			if (nfds < MAX_CLIENTS) {
-				poll_fd[nfds].fd = connected_socket;
-				poll_fd[nfds].events = POLLIN;
-				nfds++;
-				std::cout << "A new client is on the server !" << std::endl;
-			}
-			else {
-                std::cerr << "Too many clients, last one has been disconnected" << std::endl;
-                close(connected_socket);
-            }
-		}
-		for (int i = 1; i < nfds; i++) {
-
-			if (poll_fd[i].revents & POLLIN) {
-				// if a connected socket receives data, THEN we should do stuff with it
-
-				memset(buff, 0, sizeof(buff));
-				int	bytes_received = recv(poll_fd[i].fd, buff, sizeof(buff), 0); // (or read())
-
-				if (bytes_received < 0)
-					throw std::runtime_error(strerror(errno));
-
-				if (bytes_received == 0) {
-					std::cout << "client " << i << " disconnected" << std::endl;
-					close(poll_fd[i].fd);
-					poll_fd[i] = poll_fd[nfds - 1];
-					nfds--;
-					i--;
-				}
-				else {
-					std::cout << "Client says: " << buff << std::endl;
-					send(poll_fd[i].fd, buff, bytes_received + 1, 0); // (or write())
-				}
-			}
-
-		}
+		process_clients(poll_fd, &nfds);
 	}
+	
+	// is never called
 	for (int i = 1; i < nfds; i++) {
 		close(poll_fd[i].fd);
 	}
-	return (0);
+}
+
+void	start_server() {
+
+	int	server_socket;
+
+	server_socket = create_server_socket();
+	bind_server_socket(server_socket);
+	run_server(server_socket);
 }
 
 int	main() {
 
 	try {
-		global_function();
+		start_server();
 	}
 	catch (std::exception &er) {
-		std::cerr << er.what() << std::endl;
+		std::cerr << "Error on server side" << er.what() << std::endl;
 	}
 	return (0);
 }
--- todo.md
+++ todo.md
@@ -1,7 +1,7 @@
-[ ] recode basic server to understand
-[ ] separer en plusieurs fonctions pour code propre
-[ ] GET request from index.html
-[ ] POST request from index.html
+[x] recode basic server to understand
+[x] separer en plusieurs fonctions pour code propre
+[ ] GET request from index.html -> basic class
+[ ] POST request to modify index.html (or create new page)
 [ ] 404 error page
 [ ] securiser POST requests
 [ ] try to use select() instead of poll()
