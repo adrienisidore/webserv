296ec2553bf6709399d6dd9c5004f37b7158296b
Adrien Isidore <aisidore@f1br6s15.clusters.42paris.fr>
2025-10-07 18:29:36 +0200

    0.1

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -9,7 +9,7 @@ class	Request {
 
 	// start-line
 		std::string	_method;
-		std::string	_request_target;
+		std::string	_request_target;//ressource demandee
 		std::string	_protocol;
 
 	// Headers
@@ -18,7 +18,11 @@ class	Request {
 	// Body
 		std::string	_body;
 
-	// Status
+	// Status : une variable qui devrait etre propre a Request et response ? On peut
+	//On peut faire deriver Request et Response d'une Interface Message.
+	//Dans Message il faut aussi forcement un body meme s'il reste vide (requetes sans body).
+	//Idem pour header ?
+	//Idem pour protocol
 		int	_status_code;
 
 		Request();
