296acba14ab2bb5a595e6954c55ed5b99fdc77bc
Gaston Fontagneu <gfontagn@f6r3s8.clusters.42paris.fr>
2025-10-24 17:30:19 +0200

    cgis

--- includes/ServerMonitor.hpp
+++ includes/ServerMonitor.hpp
@@ -13,14 +13,19 @@ private:
 	std::vector<pollfd>				_pollfds; //all sockets we monitor, wrapped up for poll()
 	std::map<int, TCPConnection *>	_map_connections;// map all tcp_sockets to their corresponding TCP connection
 	std::map<int, ServerConfig>		_map_server_configs; // map all listening sockets to their corresponding server configs 
+	std::map<int, CGI>				_map_cgis; // map all cgis with their corresponding 
 
 	void							create_all_listening_sockets();
 	void							bind_listening_socket(int listening);
 	void							add_new_client_socket(int listening);
 	pollfd							pollfd_wrapper(int fd);
+
 	
 	std::vector<pollfd>::iterator	close_tcp_connection(std::vector<pollfd>::iterator);
+	std::vector<pollfd>::iterator	last_socket() ;
+	void							monitor_listening_sockets();
 	void							monitor_connections();//monitor the tcp_socket (client connected)
+	void							monitor_cgis();
 
 
 	static void						handle_sigint(int sig);//static pour etre accessible par signal
